1c7b5970c486faadd6ca3c76284df56f1b6e493f
Akshay Kanthi <aksanoble@gmail.com>
2018-02-21 11:06:53 +0530

    initial commit

---
+++ Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+import '../css/style.css';
+import '../css/exchange.scss';
+import App from '../components/App';
+
+
+if (module.hot) {
+    module.hot.accept(App, ()=>{
+        render(<App/>,
+        document.getElementById('app')
+        );  
+    });
+};
+
+render(<App />, document.getElementById('app'));
